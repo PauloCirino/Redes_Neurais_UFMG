ede763ff747683cae70c8906d2372566e695871d
Paulo <paulocirino.neto@gmail.com>
2017-10-08 14:27:57 -0300

    change data plot

--- TrabalhoParcial/Article/createGraphs.R
+++ TrabalhoParcial/Article/createGraphs.R
@@ -2,19 +2,80 @@ require(tidyverse)
 require(mlbench)
 source('./src/methods.R')
 
-baseImgDir <- './Article/imgs'
+baseImgDir <- './Article/imgs/'
 if(!dir.exists(baseImgDir)){
     dir.create(baseImgDir)
 }
 
-Data <- mlbench::mlbench.2dnormals(n = 500, sd = 0.55)
-X <- Data$x
-Y <- Data$classes
-Q <- getQuality(X = X, Y = as.numeric(Y) - 1)
-Data <- data.frame(X1 = X[, 1], X2 = X[, 2], Y = as.numeric(Y) - 1, Q = Q) 
+ggplot2::ggplot(data = NULL,
+                ggplot2::aes(x = X[, 1], y = X[, 2],
+                             color = log(Q), z = log(Q), alpha = log(Q) )) + 
+    ggplot2::geom_point(size = 2.5) + 
+    ggplot2::geom_density_2d(color = 'blue') +
+    ggplot2::scale_colour_continuous(low = 'green', high = 'red') +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = 'X1', y = 'X2') +
+    ggplot2::theme(axis.title.x = element_blank(),
+                   axis.text.x = element_blank(),
+                   axis.ticks.x = element_blank(),
+                   axis.title.y = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   legend.position = "none") +
+    ggplot2::ggsave(file = paste(baseImgDir, 'qualidade_plot.png',sep = ''))
 
-Data %>%
-    ggplot2::ggplot(ggplot2::aes(x = X1, y = X2, color = Q)) +
-    ggplot2::geom_point() + 
-    ggplot2::scale_color_continuous()
+ggplot2::ggplot(data = NULL,
+                ggplot2::aes(x = Q)) +
+    geom_histogram(colour = "black",
+                   fill = "white",
+                   bins = 30) +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = 'Qualidade') +
+    ggplot2::theme(axis.ticks.x = element_blank(),
+                   axis.title.y = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   legend.position = "none") +
+    ggplot2::ggsave(file = paste(baseImgDir, 'histograma.png',sep = ''))
+
+ggplot2::ggplot(data = NULL,
+                ggplot2::aes(x = ' ', y = Q)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = 'Qualidade') +
+    ggplot2::theme(axis.ticks.x = element_blank(),
+                   axis.title.y = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   legend.position = "none") +
+    ggplot2::ggsave(file = paste(baseImgDir, 'boxplot.png',sep = ''))
+
+
+
+ggplot2::ggplot(data = NULL,
+                ggplot2::aes(x = log(Q) )) +
+    geom_histogram(colour = "black",
+                   fill = "white",
+                   bins = 30) +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = 'Log(Qualidade)') +
+    ggplot2::theme(axis.ticks.x = element_blank(),
+                   axis.title.y = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   legend.position = "none") +
+    ggplot2::ggsave(file = paste(baseImgDir, 'histograma_log.png',sep = ''))
+
+
+ggplot2::ggplot(data = NULL,
+                ggplot2::aes(x = ' ', y = log(Q))) +
+    ggplot2::geom_boxplot() +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = 'Log(Qualidade)') +
+    ggplot2::theme(axis.ticks.x = element_blank(),
+                   axis.title.y = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   legend.position = "none") +
+    ggplot2::ggsave(file = paste(baseImgDir, 'boxplot_log.png',sep = ''))
 
